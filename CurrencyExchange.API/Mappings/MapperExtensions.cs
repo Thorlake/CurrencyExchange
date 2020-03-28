da847d48ae4a8d6ce02916f7398fcfe28ed054c8
Mikhail Torozerov <mtorozerov@gmail.com>
2020-03-28 23:47:01 +0300

    Add mapper extension & user profile

---
+++ CurrencyExchange.API/Mappings/MapperExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoMapper
+{
+    using System.Collections.Generic;
+
+    public static class MapperExtensions
+    {
+        public static IEnumerable<TDestination> MapMany<TDestination>(this IMapper mapper, IEnumerable<object> source)
+        {
+            return mapper.Map<IEnumerable<TDestination>>(source);
+        }
+    }
+}
---
+++ CurrencyExchange.API/Mappings/UserProfile.cs
@@ -0,0 +1,24 @@
+﻿namespace CurrencyExchange.API.Mappings
+{
+    using AutoMapper;
+    using CurrencyExchange.API.Dto.Requests.User;
+    using CurrencyExchange.API.Dto.Responses.User;
+    using CurrencyExchange.BLL.Abstractions.Services.Args;
+    using CurrencyExchange.Model.Abstractions;
+
+    public class UserRequestProfile : Profile
+    {
+        public UserRequestProfile()
+        {
+            CreateMap<CreateUserRequest, CreateUserArgs>();
+        }
+    }
+
+    public class UserResponseProfile : Profile
+    {
+        public UserResponseProfile()
+        {
+            CreateMap<IUser, UserResponse>();
+        }
+    }
+}
