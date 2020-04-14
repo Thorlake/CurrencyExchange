015de59380a300d7b4ef5e968dbcd09570eb2305
Mikhail Torozerov <mtorozerov@gmail.com>
2020-04-15 00:16:10 +0300

    Add user wallet mapping profile

---
+++ CurrencyExchange.API/Mappings/UserWalletProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace CurrencyExchange.API.Mappings
+{
+    using AutoMapper;
+    using CurrencyExchange.API.Dto.Requests.User;
+    using CurrencyExchange.API.Dto.Responses.User;
+    using CurrencyExchange.BLL.Abstractions.Services.Args;
+    using CurrencyExchange.Model.Abstractions;
+
+    public class UserWalletRequestProfile : Profile
+    {
+        public UserWalletRequestProfile()
+        {
+            CreateMap<UserWalletDepositRequest, UserWalletDepositArgs>();
+            CreateMap<UserWalletWithdrawalRequest, UserWalletWithdrawalArgs>();
+            CreateMap<UserWalletExchangeRequest, UserWalletExchangeArgs>();
+        }
+    }
+
+    public class UserWalletResponseProfile : Profile
+    {
+        public UserWalletResponseProfile()
+        {
+            CreateMap<IUserWallet, UserWalletResponse>();
+        }
+    }
+}
